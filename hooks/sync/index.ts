373987135b7458c0e2f761abe7bec69bac3729cf
Dawson <d.pope@eagles.oc.edu>
2025-05-18 00:34:20 -0500

    myb

--- hooks/sync/index.ts
+++ hooks/sync/index.ts
@@ -3,5 +3,4 @@ export { useDeviceId } from './useDeviceId';
 export { useWorkspaceId } from './useWorkspaceId';
 export { useWorkspaceDetails } from './useWorkspaceDetails';
 export { useSyncStatusLogger } from './useSyncStatusLogger';
-export { useLifecycleLogger } from './useLifecycleLogger';
 export { useLogUpdates } from './useLogUpdates';
